24aa0d78f304e36eda6914cff53477dd13a70b07
KIM DONG IL <cappu159@gmail.com>
2021-12-11 16:01:10 +0900

    Test and code checkbox functionality

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
 import React, { useState } from "react";
-import "./App.css";
+// import "./App.css";
 
 function App() {
   const [btnColor, setBtnColor] = useState("red");
+  const [disabled, setDisabled] = useState(false);
+
   const newButtonColor = btnColor === "red" ? "blue" : "red";
 
   const clickHandler = () => {
     setBtnColor(newButtonColor);
   };
 
-  console.log(`Change to ${newButtonColor}`);
-
   return (
     <div className="App">
-      <button style={{ backgroundColor: btnColor }} onClick={clickHandler}>
+      <button
+        style={{ backgroundColor: btnColor, color: "#fff" }}
+        onClick={clickHandler}
+        disabled={disabled}
+      >
         Change to {newButtonColor}
       </button>
+      <br />
+      <input
+        type="checkbox"
+        id="enable-button-checkbox"
+        defaultChecked={disabled}
+        aria-checked={disabled}
+        onChange={(e) => setDisabled(e.target.checked)}
+      />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -31,3 +31,28 @@ test("button을 클릭하면 파란색으로 바뀐다", () => {
   // expect the button text to be change to red
   expect(colorButton.textContent).toBe("Change to red");
 });
+
+test("초기 상태", () => {
+  render(<App />);
+
+  // 버튼이 활성화 된 상태로 시작하는지
+  const colorBtn = screen.getByRole("button", { name: "Change to blue" });
+  expect(colorBtn).toBeEnabled();
+
+  const checkBox = screen.getByRole("checkbox");
+  // 버튼이 활성화 되지 않은 상태로 시작하는지
+  expect(checkBox).not.toBeChecked();
+  // expect(checkBox).toBeDisabled();
+});
+
+test("첫번째 체크박스 클릭하면 비활성화, 두번째 클릭하면 활성화 되는지", () => {
+  render(<App />);
+  const colorButton = screen.getByRole("button");
+  const checkBox = screen.getByRole("checkbox");
+
+  fireEvent.click(checkBox);
+  expect(colorButton).toBeDisabled();
+
+  fireEvent.click(checkBox);
+  expect(colorButton).toBeEnabled();
+});
